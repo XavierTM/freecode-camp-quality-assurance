a75790911b8cf8037284a8f66a83996ee6aa7fb0
System Administrator <root@xavier-2.local>
2022-05-14 14:32:38 +0200

    another test passed

--- server.js
+++ server.js
@@ -6,12 +6,28 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
 
 app.set('view engine', 'pug');
+
+// console.log(process.env.SESSION_SECRET);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
