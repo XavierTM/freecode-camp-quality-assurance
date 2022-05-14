7cc65fa604ffffc187c0cbf8fde6e829509a80c9
Xavier Mukodi <xavier@xavier-2.local>
2022-05-14 13:17:57 +0200

    another test passed

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -85,14 +85,14 @@ suite('Functional Tests with Zombie.js', function () {
 
 	suite('"Famous Italian Explorers" form', function () {
 		// #5
-		test('Submit the surname "Polo" in the HTML form', function (done) {
+		test('Submit the surname "Colombo" in the HTML form', function (done) {
 			
-			browser.fill('surname', 'Polo').then(function() {
+			browser.fill('surname', 'Colombo').then(function() {
 
 				browser.pressButton('submit', function(err, res) {
 					browser.assert.success();
-					browser.assert.text('span#name', 'Marco');
-					browser.assert.text('span#surname', 'Polo');
+					browser.assert.text('span#name', 'Cristoforo');
+					browser.assert.text('span#surname', 'Colombo');
 					browser.assert.elements('span#dates', 1);
 
 					done();
