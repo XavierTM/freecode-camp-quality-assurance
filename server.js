2168cb4ab22b101ba57751bdbb983cd97018ddb1
System Administrator <root@xavier-2.local>
2022-05-14 14:20:42 +0200

    another test passed

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 'use strict';
+
+console.clear();
+
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
@@ -7,12 +10,14 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
+app.set('view engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render(__dirname + '/views/pug');
 });
 
 const PORT = process.env.PORT || 3000;
