55db7c55c1a0c1abe2fef697db53a5be780baee9
System Administrator <root@xavier-2.local>
2022-05-15 15:11:13 +0200

    another commit

--- server.js
+++ server.js
@@ -93,8 +93,41 @@ function ensureAuthenticated(req, res, next) {
       title: 'Connected to Database', 
       message: 'Please login',
       showLogin: true,
+      showRegistration: true,
     });
   });
+
+
+  app.route('/register')
+    .post((req, res, next) => {
+      userCollection.findOne({ username: req.body.username }, function(err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          userCollection.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          },
+            (err, doc) => {
+              if (err) {
+                res.redirect('/');
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          )
+        }
+      })
+    },
+      passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res, next) => {
+        res.redirect('/profile');
+      }
+    );
 
 
   app.post('/login', passport.authenticate('local', { failureRedirect: '/' }), function(req, res) {
