2f1c8314c5b0301cbf6f629396e176ecbd4acc70
System Administrator <root@xavier-2.local>
2022-05-15 14:42:44 +0200

    another commit

--- server.js
+++ server.js
@@ -11,49 +11,66 @@ const session = require('express-session');
 const passport = require('passport');
 
 
-console.log(ObjectID)
-
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
 
 app.set('view engine', 'pug');
 
-// console.log(process.env.SESSION_SECRET);
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 
+function getDbClient() {
+  return new Promise(async (resolve, reject) => {
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
+    try {
+      await myDB(resolve);
+    } catch (err) {
+      reject(err);
+    }
   });
-});
+}
+
+(async () => {
+
+
+  const client = await getDbClient();
+  const usersCollection = await client.db('database').collection('users');
+
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 
 
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
 
-app.route('/').get((req, res) => {
-  res.render(__dirname + '/views/pug', {title: 'Hello', message: 'Please login'});
-});
+  passport.deserializeUser((id, done) => {
+    usersCollection.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, null);
+    });
+  });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});
+
+
+  app.use('/public', express.static(process.cwd() + '/public'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.route('/').get((req, res) => {
+    res.render(__dirname + '/views/pug', {title: 'Connected to Database', message: 'Please login'});
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log('Listening on port ' + PORT);
+  });
+
+})();
